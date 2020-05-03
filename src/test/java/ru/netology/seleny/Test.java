82c3cf6420e6b1014a5b61f7dc57d1e3df8fdf09
Natalia Bryzgalova <bryzgalova_n@mail.ru>
2020-05-03 23:53:35 +0500

    firstTry

---
+++ src/test/java/ru/netology/seleny/Test.java
@@ -0,0 +1,20 @@
+package ru.netology.seleny;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+ class Test {
+    @org.junit.jupiter.api.Test
+    void shouldTest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[data-test-id = app-order ]");
+        form.$("[data-test-id=name] input").setValue("Мария Иванова");
+        form.$("[data-test-id=phone] input").setValue("+79001010010");
+        form.$("[data-test-id=agreement]").click();
+        $("success").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
